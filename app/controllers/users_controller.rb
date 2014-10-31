75b0991e524acecb53e9be83d8835e35eb31fcab
Katie Hezart <tori.velt19@gmail.com>
2014-10-31 10:18:08 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,19 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
    def new
     @user = User.new
   end
   
-     def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
-      redirect_back_or user
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
- get "users/new"
+ 
   
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: :get
   match '/signin',  to: 'sessions#new',         via: :get
@@ -10,7 +11,8 @@ Rails.application.routes.draw do
   match '/about', to: 'static_pages#about',     via: :get
   match '/contact', to: 'static_pages#contact', via: :get
   match '/help', to: 'static_pages#help',       via: :get
- 
+  match '/users', to: 'users#show', via: :get
+  
 
 
   # The priority is based upon order of creation: first created -> highest priority.
